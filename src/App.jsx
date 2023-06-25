faa87eeb345fca25a5199abe2464a2fdde8cfb76
Pablo <pabloesquilache@gmail.com>
2023-06-25 20:38:45 -0300

    posible solucion

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,37 @@
-import React from "react";
-//import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Menu from "./components/navigation/menu/menu";
 import Home from "./components/sections/home/home";
 import Education from "./components/sections/education/education";
 import Experience from "./components/sections/experience/experience";
 import Contact from "./components/sections/contact/contact";
-//import Spinner from "./components/spinner/Spinner";
+import Spinner from "./components/spinner/Spinner";
 
 const App = () => {
-  
-  // const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
-  // useEffect(() => {
-  //   window.onload = () => {
-  //     setTimeout(() => {
-  //       setIsLoading(false);
-  //     }, 5000);
-  //   };
-  // }, []);
+  useEffect(() => {
+    const handleLoad = () => {
+      setIsLoading(false);
+    };
+
+    window.addEventListener("DOMContentLoaded", handleLoad);
+
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+
+    return () => {
+      window.removeEventListener("DOMContentLoaded", handleLoad);
+      clearTimeout(timeout);
+    };
+  }, []);
 
   return (
     <div className="App">
-      {/* {isLoading ? (
+      {isLoading ? (
         <Spinner />
-      ) : ( )} */}
+      ) : (
         <BrowserRouter>
           <header>
             <Menu />
@@ -36,7 +43,7 @@ const App = () => {
             <Route path="/contacto" element={<Contact />} />
           </Routes>
         </BrowserRouter>
-      
+      )}
     </div>
   );
 };
