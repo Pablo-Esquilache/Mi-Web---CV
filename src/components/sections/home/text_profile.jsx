1e5e2e5c2949f61680293832d4fcb3749926b2eb
Pablo <pabloesquilache@gmail.com>
2023-06-25 21:09:50 -0300

    primer stop

--- src/components/sections/home/text_profile.jsx
+++ src/components/sections/home/text_profile.jsx
@@ -6,16 +6,16 @@ const TextProfile = () => {
       <div>
         <p>
         ¡Hola! Soy Pablo Esquilache y quiero presentarte mi página web. Aquí podrás encontrar información sobre mi formación académica, mi experiencia laboral y una sección de contacto para comunicarte conmigo.
-        </p>
+        </p><br />
         <p>
         Permíteme compartir algunos detalles sobre mí. Tengo 33 años y vivo en Carlos Tejedor, provincia de Buenos Aires. Además, soy padre de una encantadora niña de 5 años. Me considero una persona organizada y metódica, capaz de adaptarme a los cambios con facilidad.
-        </p>
+        </p><br />
         <p>
         Soy Desarrollador Front - End y Técnico en Administración Pública. He trabajado en el sector privado y en el sector público, adquiriendo conocimientos valiosos en ambos ámbitos. En la actualidad, estoy enfocado en desarrollar mi carrera en el campo de la tecnología de la información como desarrollador web. Mi objetivo es utilizar mis habilidades y conocimientos para contribuir al éxito de proyectos en este emocionante campo.
-        </p>
+        </p><br />
         <p>
         Te invito a explorar mi página web para obtener más detalles sobre mi formación y experiencia laboral. Si tienes alguna pregunta o deseas colaborar en proyectos interesantes, no dudes en contactarme. Estoy entusiasmado por establecer nuevas conexiones y formar parte de proyectos en el mundo de la tecnología.
-        </p>
+        </p><br />
         <p>
         ¡Gracias por visitar mi página web y mostrar interés en mi perfil profesional! Espero tener la oportunidad de colaborar contigo y participar en proyectos exitosos.
         </p>
