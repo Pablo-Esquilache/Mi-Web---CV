473d6508011c9f3892e34e3d3458fb879135bd43
Pablo <pabloesquilache@gmail.com>
2023-07-04 21:29:02 -0300

    nosee

--- src/components/sections/home/text_profile.jsx
+++ src/components/sections/home/text_profile.jsx
@@ -5,19 +5,41 @@ const TextProfile = () => {
     <div className="txt_container_home">
       <div>
         <p>
-        ¡Hola! Soy Pablo Esquilache y quiero presentarte mi página web. Aquí podrás encontrar información sobre mi formación académica, mi experiencia laboral y una sección de contacto para comunicarte conmigo.
-        </p><br />
+          ¡Hola! Soy Pablo Esquilache y quiero presentarte mi sitio web. Aquí
+          podrás encontrar información sobre mi formación académica, mi
+          experiencia laboral y una sección de contacto para comunicarte
+          conmigo.
+        </p>
+        <br />
         <p>
-        Permíteme compartir algunos detalles sobre mí. Tengo 33 años y vivo en Carlos Tejedor, provincia de Buenos Aires. Además, soy padre de una encantadora niña de 5 años. Me considero una persona organizada y metódica, capaz de adaptarme a los cambios con facilidad.
-        </p><br />
+          Permíteme compartir algunos detalles sobre mí. Tengo 33 años y vivo en
+          Carlos Tejedor, provincia de Buenos Aires. Además, soy padre de una
+          encantadora niña de 5 años. Me considero una persona organizada y
+          metódica, capaz de adaptarme a los cambios con facilidad.
+        </p>
+        <br />
         <p>
-        Soy Desarrollador Front - End y Técnico en Administración Pública. He trabajado en el sector privado y en el sector público, adquiriendo conocimientos valiosos en ambos ámbitos. En la actualidad, estoy enfocado en desarrollar mi carrera en el campo de la tecnología de la información como desarrollador web. Mi objetivo es utilizar mis habilidades y conocimientos para contribuir al éxito de proyectos en este emocionante campo.
-        </p><br />
+          Soy Desarrollador Front-End y Técnico en Administración Pública. He
+          trabajado tanto en el sector privado como en el sector público,
+          adquiriendo conocimientos valiosos en ambos ámbitos. En la actualidad,
+          estoy enfocado en desarrollar mi carrera en el campo de la tecnología
+          de la información como desarrollador web. Mi objetivo es utilizar mis
+          habilidades y conocimientos para contribuir al éxito de proyectos en
+          este emocionante campo.
+        </p>
+        <br />
         <p>
-        Te invito a explorar mi página web para obtener más detalles sobre mi formación y experiencia laboral. Si tienes alguna pregunta o deseas colaborar en proyectos interesantes, no dudes en contactarme. Estoy entusiasmado por establecer nuevas conexiones y formar parte de proyectos en el mundo de la tecnología.
-        </p><br />
+          Te invito a explorar mi sitio web para obtener más detalles sobre mi
+          formación y experiencia laboral. Si tienes alguna pregunta o deseas
+          colaborar en proyectos interesantes, no dudes en contactarme. Estoy
+          entusiasmado por establecer nuevas conexiones y formar parte de
+          proyectos en el mundo de la tecnología.
+        </p>
+        <br />
         <p>
-        ¡Gracias por visitar mi página web y mostrar interés en mi perfil profesional! Espero tener la oportunidad de colaborar contigo y participar en proyectos exitosos.
+          ¡Gracias por visitar mi sitio web y mostrar interés en mi perfil
+          profesional! Espero tener la oportunidad de colaborar contigo y
+          participar en proyectos exitosos.
         </p>
       </div>
     </div>
